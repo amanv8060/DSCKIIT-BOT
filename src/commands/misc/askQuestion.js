eee4c43c00a7f2945cf2b3c566801eb178478e0f
Aman Verma <verma1090aman@gmail.com>
2021-06-04 00:00:19 +0530

    askQuestion Command added

---
+++ src/commands/misc/askQuestion.js
@@ -0,0 +1,23 @@
+const data = require("../../../data/data.json");
+
+module.exports = {
+  name: "askQuestion",
+  description: "Asks a question",
+  run: async (client, message, args) => {
+    if (!message.member.hasPermission("ADMINISTRATOR"))
+      return message.reply(`Become a admin MAN`);
+    const questionChannel = message.guild.channels.cache.find(
+      (channel) => channel.id === data["questionChannelId"]
+    );
+
+    console.log(args.length);
+    if (!questionChannel) {
+      message.channel.send("Question Channel Not Found");
+    } else {
+      questionChannel.send(
+        "**Question**\n" + message.content.substr(12).trim()
+      );
+    }
+  },
+  aliases: [],
+};
